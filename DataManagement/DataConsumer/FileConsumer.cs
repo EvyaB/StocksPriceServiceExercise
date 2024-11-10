5adc616154e8ace9094d1e9de81930070d56b9f8
EvyaB <evyatarbittonwork@gmail.com>
2024-11-10 13:55:36 +0200

    Creating data source consumers for web sources and local files

---
+++ DataManagement/DataConsumer/FileConsumer.cs
@@ -0,0 +1,33 @@
+﻿using StocksPriceServiceExercise.DataReceiver.DataConsumer;
+
+namespace StocksPriceServiceExercise.DataManagement.DataConsumer
+{
+    internal class FileConsumer : IDataConsumer
+    {
+        private readonly string filePath;
+
+        public FileConsumer(string filePath)
+        {
+            this.filePath = filePath ?? throw new ArgumentNullException("filePath", "Missing path to target json file");
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            // Try to get data from filePath
+            try
+            {
+                string fileData;
+                using (var reader = new StreamReader(filePath))
+                {
+                    fileData = await reader.ReadToEndAsync();
+                }
+                return fileData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to read file {filePath} with error: {ex.Message}");
+                return "";
+            }
+        }
+    }
+}
---
+++ DataManagement/DataConsumer/WebConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using StocksPriceServiceExercise.DataReceiver.DataConsumer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksPriceServiceExercise.DataManagement.DataConsumer
+{
+    internal class WebConsumer : IDataConsumer
+    {
+        private readonly string targetUri;
+
+        public WebConsumer(string targetUri)
+        {
+            this.targetUri = targetUri ?? throw new ArgumentNullException("targetUri", "Missing URI to target");
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            // Try to get data from the web data source
+            try
+            {
+                using var client = new HttpClient();
+                var content = await client.GetStringAsync(targetUri);
+
+                return content;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to read data from {targetUri} with error: {ex.Message}");
+                return "";
+            }
+        }
+    }
+}
