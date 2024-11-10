75432120537a39dadbf57b60ace3f5cd9c1623f6
EvyaB <evyatarbittonwork@gmail.com>
2024-11-10 13:28:02 +0200

    Created empty C# console application project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+class Program
+{
+    static async Task Main()
+    {
+        // Create input reader, then configure with inputs
+        // Create public interface class for getting data from user
+        // await read inputs (forever until service closes)
+    }
+}
